db48d46a284266fba236aeb2def0d8d9a44b5b78
krtech <>
2020-06-02 19:03:49 +0900

    ADD: Gimeiを追加し、ダミーデータ対応（漢字、かな、住所）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  has_one_attached :image
+  validates :name, presence: true
+  validates :emproyee_id, presence: true, uniqueness: true,
+            length: { is: 5 }, numericality: { only_integer: true }
+  validates :email, presence: true, "valid_email_2/email": true,
+            uniqueness: { case_sensitive: false }
+  validates :tel_extention, length: { maximum: 5 }, numericality: { only_integer: true }
+  validates :tel_mobile, numericality: { only_integer: true }
+  #, numericality: { only_integer: true }, format: {with: /\A\d{11}\z/}
+  scope :sort_by_name, -> { order(:name) }
+
   def self.csv_attributes
     ["name", "emproyee_id", "email", "tel_extention", "tel_outside"]
   end
@@ -11,17 +22,6 @@ class User < ApplicationRecord
       end
     end
   end
-
-  has_one_attached :image
-  validates :name, presence: true
-  validates :emproyee_id, presence: true, uniqueness: true, limit: 2,
-            length: { is: 5 }, numericality: { only_integer: true }
-  validates :email, presence: true, "valid_email_2/email": true,
-            uniqueness: { case_sensitive: false }
-  validates :tel_extention, length: { maximum: 5 }, numericality: { only_integer: true }
-  validates :tel_outside, numericality: { only_integer: true }
-  validates :tel_mobile, numericality: { only_integer: true }, format: {with: /\A\d{11}\z/}
-  scope :sort_by_name, -> { order(:name) }
 
   def self.ransackable_attributes(auth_object = nil)
     %w[name name_kana email dept1 dept2 dept3 location_name position_name]
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,49 @@
 #
 # Examples:
 #
+require 'addressable/uri'
+
 Faker::Config.locale = 'ja'
 
+# 20.times do
+#   image = Faker::Avatar.image
+#   emproyee_id = Faker::Number.number(digits: 5)
+#   name = Faker::Name.unique.name
+#   # name = Faker::JapaneseMedia::DragonBall.unique.character
+#   email = Faker::Internet.email
+#   dept1 = Faker::Company.industry
+#   dept2 = Faker::Company.industry
+#   dept3 = Faker::Company.industry
+#   position_name = Faker::Music.genre
+#   location_name = Faker::Address.state
+#   # location_name_kana =
+#   address_name = Faker::Address.full_address
+#   # address_name_kana =
+#   zip_number = Faker::Address.zip_code
+#   tel_outside = Faker::PhoneNumber.phone_number_with_country_code
+#   tel_extention = Faker::PhoneNumber.extension
+#   tel_mobile = Faker::PhoneNumber.cell_phone.gsub(/-/, '')
+#   User.create!(image: image, emproyee_id: emproyee_id, name: name, email: email, dept1: dept1, dept2: dept2, dept3: dept3,
+#                position_name: position_name, location_name: location_name, address_name: address_name,
+#                zip_number: zip_number, tel_outside: tel_outside, tel_mobile: tel_mobile, tel_extention: tel_extention)
+# end
+
 20.times do
-  image = Faker::Avatar.image
+  gimei = Gimei.new
+  # FIXME Fakerで遊んだあと
+  # uri = Addressable::URI.parse(Faker::Avatar.image(slug = nil, size = '100x100', format = 'png'))
+  # image = uri.normalize
+  # FIXME ローカルにおいた画像をデフォルトにする
+  # image = File.open(File.join(Rails.root, "emproyee-image.png"))
   emproyee_id = Faker::Number.number(digits: 5)
-  # name = Faker::Name.unique.name
-  name = Faker::JapaneseMedia::DragonBall.unique.character
+  name = gimei.name.kanji
+  name_kana = gimei.name.hiragana
   email = Faker::Internet.email
   dept1 = Faker::Company.industry
   dept2 = Faker::Company.industry
   dept3 = Faker::Company.industry
   position_name = Faker::Music.genre
-  location_name = Faker::Address.state
+  location_name = gimei.prefecture.kanji
   # location_name_kana =
   address_name = Faker::Address.full_address
   # address_name_kana =
@@ -23,7 +53,21 @@ Faker::Config.locale = 'ja'
   tel_outside = Faker::PhoneNumber.phone_number_with_country_code
   tel_extention = Faker::PhoneNumber.extension
   tel_mobile = Faker::PhoneNumber.cell_phone.gsub(/-/, '')
-  User.create!(image: image, emproyee_id: emproyee_id, name: name, email: email, dept1: dept1, dept2: dept2, dept3: dept3,
-               position_name: position_name, location_name: location_name, address_name: address_name,
-               zip_number: zip_number, tel_outside: tel_outside, tel_mobile: tel_mobile, tel_extention: tel_extention)
+  User.create!(
+               # image: image,
+               emproyee_id: emproyee_id,
+               name: name,
+               name_kana: name_kana,
+               email: email,
+               dept1: dept1,
+               dept2: dept2,
+               dept3: dept3,
+               position_name: position_name,
+               location_name: location_name,
+               address_name: address_name,
+               zip_number: zip_number,
+               tel_outside: tel_outside,
+               tel_extention: tel_extention,
+               tel_mobile: tel_mobile
+  )
 end
